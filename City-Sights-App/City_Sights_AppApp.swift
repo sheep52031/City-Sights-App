f4857ee9ded56b4fbb49bef43530b9ebd86519ae
sheep52031 <sheep52031@gmail.com>
2022-04-12 14:04:38 +0800

    Initial Commit

---
+++ City-Sights-App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City-Sights-App
+//
+//  Created by 林家任 on 2022/4/12.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City-Sights-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City-Sights-App
+//
+//  Created by 林家任 on 2022/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
